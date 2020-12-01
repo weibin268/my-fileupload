d277a4cdc2a939b77a0858c42a1a021ba357c7dd
庄伟斌 <zhuangweibin@qq.com>
2020-12-01 11:39:24 +0800

    aa

--- my-fileupload-starter/src/main/java/com/zhuang/fileupload/autoconfigure/MyFileUploadProperties.java
+++ my-fileupload-starter/src/main/java/com/zhuang/fileupload/autoconfigure/MyFileUploadProperties.java
@@ -1,9 +1,7 @@
 package com.zhuang.fileupload.autoconfigure;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@Data
 @ConfigurationProperties(prefix = "my.fileupload")
 public class MyFileUploadProperties {
 
@@ -11,7 +9,6 @@ public class MyFileUploadProperties {
     private final Ftp ftp = new Ftp();
     private final Local local = new Local();
 
-    @Data
     public static class Ftp {
         private String ip = "127.0.0.1";
         private String userName = "root";
@@ -19,11 +16,73 @@ public class MyFileUploadProperties {
         private String basePath = "";
         //链接模式：主动=active；被动=passive；
         private String connectionMode = "active";
+
+        public String getIp() {
+            return ip;
+        }
+
+        public void setIp(String ip) {
+            this.ip = ip;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getBasePath() {
+            return basePath;
+        }
+
+        public void setBasePath(String basePath) {
+            this.basePath = basePath;
+        }
+
+        public String getConnectionMode() {
+            return connectionMode;
+        }
+
+        public void setConnectionMode(String connectionMode) {
+            this.connectionMode = connectionMode;
+        }
     }
 
-    @Data
     public static class Local {
         private String basePath = "";
+
+        public String getBasePath() {
+            return basePath;
+        }
+
+        public void setBasePath(String basePath) {
+            this.basePath = basePath;
+        }
     }
 
+    public String getStoreProvider() {
+        return storeProvider;
+    }
+
+    public void setStoreProvider(String storeProvider) {
+        this.storeProvider = storeProvider;
+    }
+
+    public Ftp getFtp() {
+        return ftp;
+    }
+
+    public Local getLocal() {
+        return local;
+    }
 }
